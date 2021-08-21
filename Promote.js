b3f5b739e2a86a878f7a936ca92347e6c91edefb
sbusiso <86268790+DKHitman1@users.noreply.github.com>
2021-08-21 10:40:03 +0200

    Create Promote.js

---
+++ Promote.js
@@ -0,0 +1,17 @@
+let handler = async (m, { conn, args }) => {
+  let users = m.mentionedJid
+  conn.groupMakeAdmin(m.chat, users)
+}
+handler.help = ['promote','admin','^', '↑'].map(v => v + ' @user')
+handler.tags = ['admin']
+handler.command = /^(promote|admin|\^|↑)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
+
+handler.admin = true
+handler.botAdmin = true
+
+handler.fail = null
